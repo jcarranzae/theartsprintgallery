da8bd60db6e3cb203434430caacc3ae46a88cd94
Your Name <you@example.com>
2025-05-07 16:39:57 +0200

    Actualiza la función GET en la API de Canny para que los parámetros sean una promesa, mejorando la forma en que se obtiene el 'id'. Se optimiza la legibilidad del código al utilizar await para desestructurar 'params'.

--- app/api/canny/[id]/route.ts
+++ app/api/canny/[id]/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params;
   //const id = params.id;
